b2723bd26bd53d1c2f2c7ba5762e1ca9f8caef66
nantr0nic <andy15givingup@gmail.com>
2024-12-08 20:35:42 -0800

    Some virtual environment changes and such -- moving to Arch Linux for dev -- etc

--- book-work/web-scraping-with-python/scratchpad.py
+++ book-work/web-scraping-with-python/scratchpad.py
@@ -1,1 +1,33 @@
 # Testing stuff out of the book
+
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+from bs4 import BeautifulSoup
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        title = bs.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title = getTitle("http://www.pythonscraping.com/pages/page1.html")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
+    
+
+"""
+print(bs.h1)
+Can also use: 
+bs.html.body.h1
+bs.body.h1
+bs.html.h1
+"""
